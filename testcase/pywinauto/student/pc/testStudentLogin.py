49218b2c3281116868b706dc89902dcc4f4436a9
weilutao <weilutao@bell.ai>
2021-06-25 16:31:16 +0800

    学生目录结构调整

--- testcase/pywinauto/student/pc/testStudentLogin.py
+++ testcase/pywinauto/student/pc/testStudentLogin.py
@@ -1,11 +1,13 @@
 from pywinauto import Application
 from config.operation_yaml import OperathionYAML
 import pytest
+import allure
 
 student_login_data = OperathionYAML().read_yaml('student_login.yaml')
 path = student_login_data['init']['path']
 
 
+@allure.feature('学生客户端登录')
 class TestStudentLogin(object):
     @classmethod
     def setup_class(cls):
