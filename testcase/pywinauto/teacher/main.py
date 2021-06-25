1476e77234c7680bf40d4b978a2e9e23de2b7d82
weilutao <weilutao@bell.ai>
2021-06-25 14:05:21 +0800

    pytest配置文件

--- testcase/pywinauto/teacher/main.py
+++ testcase/pywinauto/teacher/main.py
@@ -6,8 +6,8 @@ import pytest
 
 if __name__ == '__main__':
     # OA测试服排课
-    # pytest.main(['E:/pc_autotest/util/oa_test_course_scheduling.py'])
-    # sleep(300)
+    pytest.main(['E:/pc_autotest/util/oa_test_course_scheduling.py'])
+    sleep(300)
 
     # # 登录教师端,进入直播间
     # pytest.main(['testTeacherLogin.py'])
